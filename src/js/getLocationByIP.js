4282f6749aeef299f21a339b48916388526c78cf
Olga <olga-klgd@yandex.ru>
2024-10-23 15:52:59 +0200

    Looking for an error

--- src/js/getLocationByIP.js
+++ src/js/getLocationByIP.js
@@ -3,6 +3,6 @@ export async function getLocationByIP() {
   const res = await fetch('https://get.geojs.io/v1/ip/geo.json');
   const result = await res.json();
   const myCity = result.city;
-  console.log(myCity);
+  // console.log(myCity);
   return myCity;
 }
