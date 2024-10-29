b9c96969a948e5e6eb50c9dcd9d945b88d400a33
Olga <olga-klgd@yandex.ru>
2024-10-29 15:13:40 +0200

    change webpack.config 5

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 module.exports = {
   mode: 'development',
-  entry: './src/js/indexedDB.js',
+  entry: './src/js/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
