673b5a1cb3ec96b25a69d28ca863c79339bb9edf
Olga <olga-klgd@yandex.ru>
2024-10-30 11:43:56 +0200

    Published http://localhost:8092/

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,25 @@
+/* eslint-disable no-dupe-keys */
+/* eslint-disable no-undef */
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable import/no-unresolved */
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: './src/js/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+  module: {
+    rules: [
+      { test: /\.css$/, use: 'css-loader' },
+      { test: /\.(js)$/, use: 'babel-loader' },
+    ],
   },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin(),
+  ],
+  mode: 'production',
 };
