46de0a8ca11bb98d3e1ff277bc146e032ca98bd8
Olga <olga-klgd@yandex.ru>
2024-11-01 11:39:32 +0200

    Check UI

--- src/js/findWeather.js
+++ src/js/findWeather.js
@@ -1,13 +1,9 @@
 /* eslint-disable linebreak-style */
 // import { getLocationByIP } from './getLocationbyIP.js';
 
-// const openWeatherApiKey = 'b889a81670a5b539acbb6c0c29d0e689';
 const weatherApiKey = 'ff67773d96384e45a29113456242210';
 
 export async function findWeather(city) {
-  // const res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${openWeatherApiKey}&units=metric&lang=ru`);
-  // const weatherJson = await res.json();
-
   const res = await fetch(
     `https://api.weatherapi.com/v1/current.json?key=${weatherApiKey}&q=${city}`,
   );
